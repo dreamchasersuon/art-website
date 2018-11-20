6a862d39c92a1f44d43b4fe1fdbab6d970e92dca
Tim Zdornov <lighttimfire@MacBook-Air-Tim.local>
2018-11-20 15:37:53 +0300

    Make changes in script gallery.

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,5 +1,5 @@
 const onClickButton = () => {
-	let button = document.getElementsByClassName('button');
+	let button = document.getElementsByClassName('button')[0];
 	button.onclick = () => {
 		document.getElementsByClassName('gallery').style.display = 'flex';
 	}
